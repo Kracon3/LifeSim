ef1f65a7419601ddbf207e5aafafabbe5aeac363
Kracon3 <Kracon7136@gmail.com>
2023-04-24 21:08:44 -0600

    replaced drawRectangle
    - replaced drawRectangle with graphics' built in drawRect

--- src/sim/view/LifePanel.java
+++ src/sim/view/LifePanel.java
@@ -5,12 +5,14 @@ import sim.view.SimPanel;
 import sim.model.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
 public class LifePanel extends JPanel
 {
 	private Controller app;
 	private SimPanel simPanel;
+	private Graphics graphics;
 	
 	public LifePanel(Controller app)
 	{
@@ -45,19 +47,15 @@ public class LifePanel extends JPanel
 		for(int index = 0; index < creatureNum; index++)
 		{
 			creatures[index] = new Creature(0, 10, index * 10);
-			drawRectangle(creatures[index].getXPosition(), creatures[index].getYPosition(), 5, 5, Color.GREEN);
+			graphics.drawRect(creatures[index].getXPosition(), creatures[index].getYPosition(), 5, 5);
 		}
 		
 		for(int index = 0; index < plantNum; index++)
 		{
 			plants[index] = new Plant((int) (Math.random()) * 10, (int) (Math.random() * 800), (int) (Math.random() * 800));
-			drawRectangle(plants[index].getXPosition(), plants[index].getYPosition(), 5, 5, Color.GREEN);
+			graphics.drawRect(plants[index].getXPosition(), plants[index].getYPosition(), 5, 5);
 		}
 	}
 	
-	private void drawRectangle(int xPosition, int yPosition, int width, int height, Color rectColor)
-	{
-		
-	}
 	
 }
